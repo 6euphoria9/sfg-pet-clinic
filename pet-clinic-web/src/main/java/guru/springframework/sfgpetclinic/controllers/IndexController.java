f7076b47347955f85f8161b91e7f6c05ec31d965
Daniel <elquarry23@gmail.com>
2022-09-08 17:03:33 +0200

    adding index.html page and indexController. closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String indexPage() {
+
+
+        return "index";
+    }
+}
